95d0b7bef98e175d6bd9858fa903865cfc8e0a5d
inoha_kudo <inoha1062@gmail.com>
2026-02-01 17:53:21 +0900

    [add] Helloモジュール(Vue.js依存)の追加

---
+++ resources/js/components/HelloContent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+    message: string;
+}>();
+</script>
+
+<template>
+    <h1>Hello, {{ message }}!</h1>
+</template>
---
+++ resources/js/components/index.ts
@@ -0,0 +1,1 @@
+export { default as HelloContent } from './HelloContent.vue';
